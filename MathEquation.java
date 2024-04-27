207776175e4039fea4e43de373ec8d8b47d6321d
Kendry-Cabrera <157192486+Kendry-Cabrera@users.noreply.github.com>
2024-04-26 21:36:36 -0400

    Kendry

--- MathEquation.java
+++ MathEquation.java
@@ -1,4 +1,7 @@
-
+public static double integrate(double x, double a, double b) {
+        // Implementación de la integral definida de x^a + bx
+        return (Math.pow(x, a + 1) / (a + 1)) + (b * x);
+}
         return (a * Math.pow(x, a - 1)) + b;
     
 
